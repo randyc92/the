daf64c3a55c577fefb7c7339de200c19225a7465
randyc92 <costarandy92@gmail.com>
2024-08-11 21:32:32 -0400

    test

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+d = "Dg"
